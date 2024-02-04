faef94aaedbcac43950eabe2f49734dd53559c1c
xknows <alikng94@gmail.com>
2024-02-04 13:43:18 +0330

    error handled and some improvement

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path"
@@ -13,17 +14,41 @@ var link string
 func main() {
 
 	fmt.Println("input link :")
-	fmt.Scanln(&link)
+	_, err := fmt.Scanln(&link)
+	if err != nil {
+		log.Fatal(err)
+	}
 	download(link)
 }
 func download(link string) {
-	res, _ := http.Get(link)
-	file, _ := os.Create(path.Base(link))
+	res, err := http.Get(link)
+	fmt.Printf("Starting \n")
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(res.Body)
+	
+	file, err := os.Create(path.Base(link))
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	size, err := io.Copy(file, res.Body)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Printf("%dkb \n", size/1000)
-	fmt.Printf("finished")
-	defer res.Body.Close()
+	
+	sizeKb := size / 1000
+	sizeMb := sizeKb / 1000
+
+	if size < 1024 {
+		fmt.Printf("Finished. size: %db", size)
+	} else if sizeKb < 1024 {
+		fmt.Printf("Finished. size: %dKb", sizeKb)
+	} else if sizeKb >= 1024 {
+		fmt.Printf("Finished. size: %dMb", sizeMb)
+	}
+
 }
