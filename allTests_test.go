af523e767a1419090d3b102f0f1df9aa6fa70a93
xknows <alikng94@gmail.com>
2024-02-08 22:18:48 +0330

    rewritten all tests

--- allTests_test.go
+++ allTests_test.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"github.com/stretchr/testify/assert"
 	"net/http"
-	"os"
 	"testing"
 	"time"
 )
@@ -12,30 +10,10 @@ import (
 const linkForTest = "https://golikhanoom.ir/wp-content/uploads/2023/11/aks-gol-v-manzare-ziba-baraye-porofail.jpg"
 const fileFotTest = "test.txt"
 
-func TestFirstPart(t *testing.T) {
-	_, _, err := download(linkForTest)
-	assert.Equal(t, nil, err)
-}
-
-func TestSecondPart(t *testing.T) {
+func TestMerged(t *testing.T) {
 	res, _ := http.Get(linkForTest)
 	someNumber := time.Duration(124)
 
-	err := speed(res.ContentLength, someNumber)
-	assert.Equal(t, nil, err)
-}
-
-func TestCli1(t *testing.T) {
-	options = 2
-	if options == 1 {
-		_, _, err := download(linkForTest)
-		assert.Equal(t, nil, err)
-	} else if options == 2 {
-		reader, _ := os.Open(fileFotTest)
-		scanner := bufio.NewScanner(reader)
-		for scanner.Scan() {
-			_, _, err := download(scanner.Text())
-			assert.Equal(t, nil, err)
-		}
-	}
+	_, error := speed(res.ContentLength, someNumber)
+	assert.Equal(t, nil, error)
 }
